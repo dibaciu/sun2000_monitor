c1add287836eae079a7ffa7043de72d10d083d62
Mihai Baciu <mbaciu@gopro.com>
2025-12-26 22:50:58 +0200

    Don't bail out when inverter is not reachable.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sun2000 import Sun2000
 from influxdb_client_3 import Point
 import logging
 
+from pymodbus.exceptions import ModbusIOException
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
@@ -18,8 +20,11 @@ logger.info(f'Sun2000 ping server: {sun2000_client.ping()}')
 logger.info(f'Polling every {config.polling_interval_seconds} seconds')
 
 while True:
-    points = [
-        Point(influxdb_client.config.influxdb_dbname).tag("source", register_data.source).field(attribute, register_data.value) for attribute, register_data in sun2000_client.poll_all().items()
-    ]
-    influxdb_client.client.write(points, )
+    try:
+        points = [
+            Point(influxdb_client.config.influxdb_dbname).tag("source", register_data.source).field(attribute, register_data.value) for attribute, register_data in sun2000_client.poll_all().items()
+        ]
+        influxdb_client.client.write(points, )
+    except ModbusIOException as e:
+        logger.error(e)
     time.sleep(config.polling_interval_seconds)
